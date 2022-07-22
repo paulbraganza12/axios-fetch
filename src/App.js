12e271b98dfdbe1eed1cb7dfff76e428f8ebe9d5
paulbraganza12 <paulbraganza12@gmail.com>
2022-07-22 16:31:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import useFetch from "./useFetch";
 
 function App() {
-  const { data } = useFetch("https://images-api.nasa.gov/search?q=moon");
+  const { data, loading } = useFetch(
+    "https://images-api.nasa.gov/search?q=moon"
+  );
 
   const imageArray = data?.items
     ?.map((item) => item.links?.map((item) => item.href))
     .flat();
   return (
     <div className="App">
-      {imageArray.map((item, index) => {
-        return <img src={item} alt="image-moon" key={index} />;
-      })}
+      {!loading &&
+        imageArray?.map((item, index) => {
+          return <img src={item} alt="image-moon" key={index} />;
+        })}
     </div>
   );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
